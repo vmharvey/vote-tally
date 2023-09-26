d2e16b05a990e4534a7a0c2be31d97ff97bbefe5
ibarnsley <a1765822@student.adelaide.edu.au>
2023-09-26 16:04:40 +0930

    Issue #4 Resolved added data reader

--- vote_tally/vote_tally.py
+++ vote_tally/vote_tally.py
@@ -1,3 +1,24 @@
+#! usr/bin/env python 
+"""
+Determine a board for a committee from a set of votes
+"""
+
+import pandas as pd
+
+def read_votes():
+    """
+    Read votes in from a csv file into a data frame
+    """
+    
+    votes_df = pd.read_csv ('data/test_votes.csv')
+
+    return votes_df
 
 def main():
+    votes_df = read_votes()
+    print(votes_df)
     print("test main")
+
+if __name__ == "__main__":
+    main()
+
